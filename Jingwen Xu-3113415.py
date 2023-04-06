2c4404992fcc242a5b535989470b238717173269
JingwenXu3113415 <129739921+JingwenXu3113415@users.noreply.github.com>
2023-04-06 10:42:38 +0200

    Add files via upload

---
+++ Jingwen Xu-3113415.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(0,100)
+guess = 0
+count = 0
+
+while guess != number and guess != "exit":
+    guess = input("please guess a nmber between 1 and 100. when you want to end the game print 'exit':")
+    if guess == "exit":
+        print("game over")
+        break
+    guess = int(guess)
+    count += 1
+    if guess not in range(1,100):
+        print("You have to guess a number between 1 and 100!")
+    elif guess < number:
+        print("You've guessed too low!")
+    elif guess > number:
+        print("You've guessed too high!")
+    else:
+        print("You've got it! It took you {} tries!".format(count))
+        break
+input("Please enter any key to exit")
